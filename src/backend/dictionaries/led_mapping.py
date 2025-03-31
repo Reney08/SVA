8119a71e64432358fca536fbfe8efb3e0ff1355d
Rene <Rene.Mlodoch@gmail.com>
2025-03-31 14:01:11 +0200

    Create led_mapping.py

---
+++ src/backend/dictionaries/led_mapping.py
@@ -0,0 +1,27 @@
+# Mapping for positions to top-row and bottom-row LEDs
+led_mapping = {
+    "Pos1": {
+        "top-row": [78, 79, 80, 81, 82],
+        "bottom-row": [71, 70, 69, 68, 67]
+    },
+    "Pos2": {
+        "top-row": [84, 85, 86, 87, 88],
+        "bottom-row": [65, 64, 63, 62, 61]
+    },
+    "Pos3": {
+        "top-row": [90, 91, 92, 93, 94],
+        "bottom-row": [59, 58, 57, 56, 55]
+    },
+    "Pos4": {
+        "top-row": [96, 97, 98, 99, 100],
+        "bottom-row": [53, 52, 51, 50, 49]
+    },
+    "Pos5": {
+        "top-row": [102, 103, 104, 105, 106],
+        "bottom-row": [47, 46, 45, 44, 43]
+    },
+    "Pos6": {
+        "top-row": [108, 109, 110, 111, 112],
+        "bottom-row": [41, 40, 39, 38, 37]
+    }
+}
