d03b8e70ae9ef2eeff491026551bb8d097dd9f75
Rene <Rene.Mlodoch@gmail.com>
2025-04-07 10:20:47 +0200

    Update executeSequence.py

--- src/backend/helpers/executeSequence.py
+++ src/backend/helpers/executeSequence.py
@@ -3,8 +3,8 @@
 # from .moveable import pump
 # from .moveable import scale
 # from src.backend.moveable.pcadevice import PCADevice
-from src.backend.moveable.servo import ServoMotor
-from src.backend.moveable.stepper import Stepper
+from moveable.servo import ServoMotor
+from moveable.stepper import Stepper
 
 
 # import sequenceHelper
