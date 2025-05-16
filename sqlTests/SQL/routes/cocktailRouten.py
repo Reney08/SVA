692cb2d29c3ffa3480296269dff04332c6cc2185
Rene <Rene.Mlodoch@gmail.com>
2025-05-16 14:41:28 +0200

    Update cocktailRouten.py

--- sqlTests/SQL/routes/cocktailRouten.py
+++ sqlTests/SQL/routes/cocktailRouten.py
@@ -2,14 +2,10 @@
 from flask import Blueprint, request, jsonify, render_template
 from db_helpers import get_db, get_cocktail_db
 
-cocktail_bp = Blueprint('zapfstelle', __name__)
+cocktail_bp = Blueprint('zapfstelle', __name__, ) # hier noch den Routenpräfix
 
 # -------------------- Seitenrouten --------------------
 '''
 Seitenrouten sind für die Darstellung von HTML-Seiten zuständig.
 '''
 
-# -------------------- API-Routen --------------------
-'''
-API-Routen sind für die Bereitstellung von Daten im JSON-Format zuständig.
-'''
