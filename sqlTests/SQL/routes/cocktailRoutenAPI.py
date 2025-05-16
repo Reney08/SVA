51a88c0e97ff09690f179e459098f2ecd2bdf2bd
Rene <Rene.Mlodoch@gmail.com>
2025-05-16 14:41:41 +0200

    Update cocktailRoutenAPI.py

--- sqlTests/SQL/routes/cocktailRoutenAPI.py
+++ sqlTests/SQL/routes/cocktailRoutenAPI.py
@@ -1,1 +1,11 @@
 
+# Routen/coktailRouten.py
+from flask import Blueprint, request, jsonify, render_template
+from db_helpers import get_db, get_cocktail_db
+
+cocktail_bp = Blueprint('zapfstelle', __name__, ) # hier noch den Routenpräfix
+
+# -------------------- API-Routen --------------------
+'''
+API-Routen sind für die Bereitstellung von Daten im JSON-Format zuständig.
+'''
